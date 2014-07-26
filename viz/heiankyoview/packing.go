ac8460e01ead436a955006e8ab37029261059964
Akira Hayakawa <ruby.wktk@gmail.com>
2014-07-26 22:16:16 +0900

    fix
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- viz/heiankyoview/packing.go
+++ viz/heiankyoview/packing.go
@@ -2,14 +2,18 @@ package heiankyoview
 
 import "sort"
 
-type val int   // Coordinate value
-type index int // Index
+type Val int   // Coordinate Value
+type Index int // Index
 
 type Rectangle struct {
-	x val
-	y val
-	w val
-	h val
+	x Val
+	y Val
+	w Val
+	h Val
+}
+
+func mkR(w, h Val) *Rectangle {
+	return &Rectangle{w: w, h: h}
 }
 
 type Packing struct {
@@ -18,61 +22,61 @@ type Packing struct {
 func (p *Packing) add(r *Rectangle) {
 }
 
-func binarySearch(xs []val, n val) index {
+func BinarySearch(xs []Val, n Val) Index {
 	i := sort.Search(len(xs), func(i int) bool { return xs[i] >= n })
 	if i < len(xs) && xs[i] == n {
-		return index(i)
+		return Index(i)
 	} else {
-		return index(-1)
+		return Index(-1)
 	}
 }
 
 type Coordinate struct {
-	xs []val
+	xs []Val
 }
 
-func NewCoordinate(minLine, maxLine val) *Coordinate {
-	c := Coordinate{[]val{minLine, maxLine}}
+func NewCoordinate(minLine, maxLine Val) *Coordinate {
+	c := Coordinate{[]Val{minLine, maxLine}}
 	return &c
 }
 
-func (c *Coordinate) get(n index) val {
+func (c *Coordinate) get(n Index) Val {
 	return c.xs[n]
 }
 
-func (c *Coordinate) has(line val) bool {
+func (c *Coordinate) has(line Val) bool {
 	return false // TODO
 }
 
 // Insertion is costful but there's no way but this
 // It only happens per adding a rectangle thus not frequent
-func (c *Coordinate) insert(at index, line val) {
+func (c *Coordinate) insert(at Index, line Val) {
 	before := c.xs[:at]
 	before = append(before, line)
 	after := c.xs[at:]
 	c.xs = append(before, after...) // I don't know how efficient this is
 }
 
-func (c *Coordinate) indexOf(line val) index {
-	return binarySearch(c.xs, line)
+func (c *Coordinate) IndexOf(line Val) Index {
+	return BinarySearch(c.xs, line)
 }
 
-func (c *Coordinate) minLine() val {
+func (c *Coordinate) minLine() Val {
 	return c.xs[0]
 }
 
-func (c *Coordinate) maxLine() val {
+func (c *Coordinate) maxLine() Val {
 	return c.xs[len(c.xs)-1]
 }
 
-func (c *Coordinate) width() val {
+func (c *Coordinate) width() Val {
 	return c.maxLine() - c.minLine()
 }
 
-func (c *Coordinate) getRightIntersection(i index, line val) (index, index) {
+func (c *Coordinate) getRightIntersection(i Index, line Val) (Index, Index) {
 	return 0, 0 // TODO
 }
 
-func (c *Coordinate) getLeftIntersection(i index, line val) (index, index) {
+func (c *Coordinate) getLeftIntersection(i Index, line Val) (Index, Index) {
 	return 0, 0 // TODO
 }
