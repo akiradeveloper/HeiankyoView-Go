fb0e408b44245dacc348e351a09d7d33be4c0d8a
Akira Hayakawa <ruby.wktk@gmail.com>
2014-07-26 17:37:13 +0900

    add Coordinate
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- viz/heiankyoview/packing.go
+++ viz/heiankyoview/packing.go
@@ -1,1 +1,78 @@
 package heiankyoview
+
+import "sort"
+
+type val int   // Coordinate value
+type index int // Index
+
+type Rectangle struct {
+	x val
+	y val
+	w val
+	h val
+}
+
+type Packing struct {
+}
+
+func (p *Packing) add(r *Rectangle) {
+}
+
+func binarySearch(xs []val, n val) index {
+	i := sort.Search(len(xs), func(i int) bool { return xs[i] >= n })
+	if i < len(xs) && xs[i] == n {
+		return index(i)
+	} else {
+		return index(-1)
+	}
+}
+
+type Coordinate struct {
+	xs []val
+}
+
+func NewCoordinate(minLine, maxLine val) *Coordinate {
+	c := Coordinate{[]val{minLine, maxLine}}
+	return &c
+}
+
+func (c *Coordinate) get(n index) val {
+	return c.xs[n]
+}
+
+func (c *Coordinate) has(line val) bool {
+	return false // TODO
+}
+
+// Insertion is costful but there's no way but this
+// It only happens per adding a rectangle thus not frequent
+func (c *Coordinate) insert(at index, line val) {
+	before := c.xs[:at]
+	before = append(before, line)
+	after := c.xs[at:]
+	c.xs = append(before, after...) // I don't know how efficient this is
+}
+
+func (c *Coordinate) indexOf(line val) index {
+	return binarySearch(c.xs, line)
+}
+
+func (c *Coordinate) minLine() val {
+	return c.xs[0]
+}
+
+func (c *Coordinate) maxLine() val {
+	return c.xs[len(c.xs)-1]
+}
+
+func (c *Coordinate) width() val {
+	return c.maxLine() - c.minLine()
+}
+
+func (c *Coordinate) getRightIntersection(i index, line val) (index, index) {
+	return 0, 0 // TODO
+}
+
+func (c *Coordinate) getLeftIntersection(i index, line val) (index, index) {
+	return 0, 0 // TODO
+}
--- viz/heiankyoview/packing_test.go
+++ viz/heiankyoview/packing_test.go
@@ -1,1 +1,69 @@
 package heiankyoview
+
+import (
+	"sort"
+	"testing"
+)
+
+func mkR(w, h val) *Rectangle {
+	return &Rectangle{w: w, h: h}
+}
+
+func assertR(t *testing.T, x, y val, r *Rectangle) {
+	if r.x != x || r.y != y {
+		t.Errorf("expected x(%d) y(%d) but actual x(%d) y(%d)", x, y, r.x, r.y)
+	}
+}
+
+func TestTest(t *testing.T) {
+	// Test assertR
+	assertR(t, 0, 0, mkR(2, 2))
+}
+
+func TestSearchInts(t *testing.T) {
+	arr := []int{1, 3}
+
+	// SearchInts - Find the index to best insert the element
+
+	if sort.SearchInts(arr, 0) != 0 {
+		t.Fail()
+	}
+	if sort.SearchInts(arr, 1) != 0 {
+		t.Fail()
+	}
+	if sort.SearchInts(arr, 2) != 1 {
+		t.Fail()
+	}
+	if sort.SearchInts(arr, 3) != 1 {
+		t.Fail()
+	}
+}
+
+func TestSearch(t *testing.T) {
+	arr := []int{1, 3, 6, 10}
+
+	// Search - Find the index where the given predicate first turns true
+	// using binary search algorithm.
+
+	if sort.Search(len(arr), func(i int) bool { return arr[i] >= 1 }) != 0 {
+		t.Fail()
+	}
+
+	if sort.Search(len(arr), func(i int) bool { return arr[i] >= 3 }) != 1 {
+		t.Fail()
+	}
+
+	if sort.Search(len(arr), func(i int) bool { return arr[i] >= 2 }) != 1 {
+		t.Fail()
+	}
+}
+
+func TestBinarySearch(t *testing.T) {
+	arr := []val{1, 3, 6, 10}
+	if binarySearch(arr, 1) != 0 {
+		t.Fail()
+	}
+	if binarySearch(arr, 2) != -1 {
+		t.Fail()
+	}
+}
